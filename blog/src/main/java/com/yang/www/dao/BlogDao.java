df9fd309049672187c2d9878d2dad47cfe6529ba
yang <1224213712@qq.com>
2017-08-28 17:09:36 +0800

    add BlogDao.java

--- blog/src/main/java/com/yang/www/dao/BlogDao.java
+++ blog/src/main/java/com/yang/www/dao/BlogDao.java
@@ -1,4 +1,29 @@
 package com.yang.www.dao;
 
+import com.yang.www.po.Blog;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Map;
+@Repository
 public interface BlogDao {
+
+    Integer add(Blog blog);
+
+    int delete(Integer blogId);
+
+    int update(Blog blog);
+
+    Integer getBlogByTypeId(Integer typeId);
+
+    Blog getLastBlog(Integer blogId);
+
+    Blog getNextBlog(Integer blogId);
+
+    Blog findById(Integer blogId);
+
+    List<Blog> groupByDate();
+
+    List<Blog> getList(Map<String,Object> map);
+
+    Long getTotal(Map<String,Object> map);
 }
