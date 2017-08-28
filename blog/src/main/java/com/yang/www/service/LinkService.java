13bc153510f7ada101581bb163c132cbe90bb1ef
yang <1224213712@qq.com>
2017-08-28 18:03:24 +0800

    add LinkService

--- blog/src/main/java/com/yang/www/service/LinkService.java
+++ blog/src/main/java/com/yang/www/service/LinkService.java
@@ -1,4 +1,17 @@
 package com.yang.www.service;
 
+import com.yang.www.po.Link;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
+
 public interface LinkService {
+    List<Link> getLinks(Map<String,Object> map);
+
+    void delete(String ids, HttpServletResponse response);
+
+    void save(Link link,HttpServletResponse response);
+
+    void list(Integer page,Integer rows,HttpServletResponse response);
 }
