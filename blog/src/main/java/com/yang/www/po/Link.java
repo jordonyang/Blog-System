d6d194378658d9b7495f73be415d25eda908e6b9
yang <1224213712@qq.com>
2017-08-28 17:07:21 +0800

    add Link.java

--- blog/src/main/java/com/yang/www/po/Link.java
+++ blog/src/main/java/com/yang/www/po/Link.java
@@ -1,4 +1,53 @@
 package com.yang.www.po;
 
 public class Link {
+    private Integer linkId;
+    private String linkName;
+    private String linkPath;
+    private Integer ordinal;
+
+    public Link() {
+    }
+
+    public Integer getLinkId() {
+        return linkId;
+    }
+
+    public void setLinkId(Integer linkId) {
+        this.linkId = linkId;
+    }
+
+    public String getLinkName() {
+        return linkName;
+    }
+
+    public void setLinkName(String linkName) {
+        this.linkName = linkName;
+    }
+
+    public String getLinkPath() {
+        return linkPath;
+    }
+
+    public void setLinkPath(String linkPath) {
+        this.linkPath = linkPath;
+    }
+
+    public Integer getOrdinal() {
+        return ordinal;
+    }
+
+    public void setOrdinal(Integer ordinal) {
+        this.ordinal = ordinal;
+    }
+
+    @Override
+    public String toString() {
+        return "Link{" +
+                "linkId=" + linkId +
+                ", linkName='" + linkName + '\'' +
+                ", linkPath='" + linkPath + '\'' +
+                ", ordinal=" + ordinal +
+                '}';
+    }
 }
