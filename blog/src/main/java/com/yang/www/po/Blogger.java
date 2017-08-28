3a0183681c02baadc72761bd1d808264453ab34e
yang <1224213712@qq.com>
2017-08-28 17:05:03 +0800

    add Blogger.java

--- blog/src/main/java/com/yang/www/po/Blogger.java
+++ blog/src/main/java/com/yang/www/po/Blogger.java
@@ -1,4 +1,92 @@
 package com.yang.www.po;
 
 public class Blogger {
+    private Integer id;
+    private String name;
+    private String password;
+    private String profile;
+    private String nickname;
+    private String signature;
+    private String imagePath;
+
+    public Blogger() {
+    }
+
+    public Blogger(String password) {
+        this.password = password;
+    }
+
+    public Blogger(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    @Override
+    public String toString() {
+        return "Blogger{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", profile='" + profile + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", signature='" + signature + '\'' +
+                ", imagePath='" + imagePath + '\'' +
+                '}';
+    }
 }
