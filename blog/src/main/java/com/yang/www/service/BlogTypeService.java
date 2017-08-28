48d9ba68f5fbab475cef669f6f8fca56d0827738
yang <1224213712@qq.com>
2017-08-28 18:02:31 +0800

    add BlogTypeService

--- blog/src/main/java/com/yang/www/service/BlogTypeService.java
+++ blog/src/main/java/com/yang/www/service/BlogTypeService.java
@@ -1,4 +1,23 @@
 package com.yang.www.service;
 
-public class BlogTypeService {
+import com.yang.www.po.BlogType;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
+
+public interface BlogTypeService {
+
+    int add(BlogType blogType);
+
+    void delete(String ids,HttpServletResponse response);
+
+    int update(BlogType blogType);
+
+    void save(BlogType blogType,HttpServletResponse response);
+
+    List<BlogType> getBlogTypes();
+
+    void list(Integer page,Integer rows,HttpServletResponse response);
 }
