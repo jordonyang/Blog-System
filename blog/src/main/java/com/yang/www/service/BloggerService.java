eefd8f4e2424b6a5ea29482ea352a41fc48b29f1
yang <1224213712@qq.com>
2017-08-28 18:00:59 +0800

    add BloggerService

--- blog/src/main/java/com/yang/www/service/BloggerService.java
+++ blog/src/main/java/com/yang/www/service/BloggerService.java
@@ -1,4 +1,20 @@
 package com.yang.www.service;
 
+import com.yang.www.po.Blogger;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public interface BloggerService {
+
+    Blogger getBloggerByName(String name);
+
+    Blogger find();
+
+    void update(Blogger blogger, HttpServletResponse response);
+
+    String login(Blogger blogger, HttpServletRequest request);
+
+    void save(MultipartFile imageFile, Blogger blogger, HttpServletRequest request, HttpServletResponse response);
 }
