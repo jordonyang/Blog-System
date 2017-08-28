b50e4d93fd7f6eb015925899c27da29425e9c697
yang <1224213712@qq.com>
2017-08-28 17:11:39 +0800

    add CommentDao.java

--- blog/src/main/java/com/yang/www/dao/CommentDao.java
+++ blog/src/main/java/com/yang/www/dao/CommentDao.java
@@ -1,4 +1,19 @@
 package com.yang.www.dao;
 
+import com.yang.www.po.Comment;
+import org.springframework.stereotype.Repository;
+import java.util.Map;
+import java.util.List;
+@Repository
 public interface CommentDao {
+
+    int add(Comment comment);
+
+    int delete(Integer id);
+
+    int update(Comment comment);
+
+    Long getTotal(Map<String,Object> map);
+
+    List<Comment> getComments(Map<String,Object> map);
 }
