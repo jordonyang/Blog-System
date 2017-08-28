8e66e69a47b8db145c96420041d074c3686e7ca1
yang <1224213712@qq.com>
2017-08-28 17:10:16 +0800

    add BloggerDao.java

--- blog/src/main/java/com/yang/www/dao/BloggerDao.java
+++ blog/src/main/java/com/yang/www/dao/BloggerDao.java
@@ -1,4 +1,14 @@
 package com.yang.www.dao;
 
+import com.yang.www.po.Blogger;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface BloggerDao {
+
+     int update(Blogger blogger);
+
+     Blogger getBloggerByName(String name);
+
+     Blogger find();
 }
