ac8c3bb843d41c1fc5e8079de8e402a05997b184
yang <1224213712@qq.com>
2017-08-28 18:02:59 +0800

    add CommentService

--- blog/src/main/java/com/yang/www/service/CommentService.java
+++ blog/src/main/java/com/yang/www/service/CommentService.java
@@ -1,4 +1,24 @@
 package com.yang.www.service;
 
+import com.yang.www.po.Comment;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Map;
+
 public interface CommentService {
+
+    void examine(String ids,Integer state,HttpServletResponse response);
+
+    void list(Integer page,Integer rows,Integer state,HttpServletResponse response);
+
+    void delete(String ids,HttpServletResponse response);
+
+    List<Comment> getComments(Map<String,Object> map);
+
+    int add(Comment comment);
+
+    void save(Comment comment, String imageCode, HttpServletRequest request, HttpServletResponse response, HttpSession session);
 }
