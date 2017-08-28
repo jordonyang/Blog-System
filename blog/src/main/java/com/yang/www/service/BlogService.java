d8aad142d5bbd7fa9fd450fc5d0c86224f661107
yang <1224213712@qq.com>
2017-08-28 18:01:23 +0800

    add BlogService

--- blog/src/main/java/com/yang/www/service/BlogService.java
+++ blog/src/main/java/com/yang/www/service/BlogService.java
@@ -1,4 +1,34 @@
 package com.yang.www.service;
 
+import com.yang.www.po.Blog;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
+
 public interface BlogService {
+
+    void delete(String blogIds,HttpServletResponse response);
+
+    void save(Blog blog,HttpServletResponse response);
+
+    Blog getLastBlog(Integer blogId);
+
+    Blog getNextBlog(Integer blogId);
+
+    Blog findById(Integer blogId);
+
+    int update(Blog blog);
+
+    Integer add(Blog blog);
+
+    Long getTotal(Map<String,Object> map);
+
+    List<Blog> groupByDate();
+
+    void getBackendList(Integer page, Integer rows, String title, HttpServletResponse response);
+
+    ModelAndView getList(Integer page,Integer typeId,String dateStr,HttpServletRequest request);
 }
