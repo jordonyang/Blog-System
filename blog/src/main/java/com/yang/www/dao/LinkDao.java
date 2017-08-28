5b8660a1c707438f2a07b28998f4e009388da240
yang <1224213712@qq.com>
2017-08-28 17:12:14 +0800

    add LinkDao.java

--- blog/src/main/java/com/yang/www/dao/LinkDao.java
+++ blog/src/main/java/com/yang/www/dao/LinkDao.java
@@ -2,7 +2,20 @@ package com.yang.www.dao;
 
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Map;
+import com.yang.www.po.Link;
 @Repository
-public class BlogrollDao {
-    List<Blogroll> getBlogrollList();
+public interface LinkDao {
+
+    int add(Link link);
+
+    int delete(Integer linkId);
+
+    int update(Link link);
+
+    Long getTotal(Map<String,Object> map);
+
+    List<Link> getLinks(Map<String,Object> map);
+
+    List<Link> list(Map<String,Object> map);
 }
