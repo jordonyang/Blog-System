43db71e9c733be837456649dd27573eee7eb3b73
yang <1224213712@qq.com>
2017-08-28 17:11:11 +0800

    add BlogTypeDao.java

--- blog/src/main/java/com/yang/www/dao/BlogTypeDao.java
+++ blog/src/main/java/com/yang/www/dao/BlogTypeDao.java
@@ -1,4 +1,24 @@
 package com.yang.www.dao;
 
+import com.yang.www.po.BlogType;
+import org.springframework.stereotype.Repository;
+import java.util.Map;
+import java.util.List;
+
+@Repository
 public interface BlogTypeDao {
+
+    int add(BlogType blogType);
+
+    int delete(Integer typeId);
+
+    int update(BlogType blogType);
+
+    Long getTotal(Map<String,Object>map);
+
+    BlogType findById(Integer typeId);
+
+    List<BlogType> getBlogTypes();
+
+    List<BlogType> list(Map<String,Object>map);
 }
