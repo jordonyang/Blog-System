798f4b16ebdab93439ad1e4ccb1eb0329188a9f1
yang <1224213712@qq.com>
2017-08-29 08:44:31 +0800

    build StringUtil

--- blog/src/main/java/com/yang/www/utils/StringUtil.java
+++ blog/src/main/java/com/yang/www/utils/StringUtil.java
@@ -1,20 +1,12 @@
-package com.java1234.util;
+package com.yang.www.utils;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 字符串工具类
- * @author 
- *
- */
+
 public class StringUtil {
 
-	/**
-	 * 判断是否是空
-	 * @param str
-	 * @return
-	 */
+
 	public static boolean isEmpty(String str){
 		if(str==null||"".equals(str.trim())){
 			return true;
@@ -23,11 +15,7 @@ public class StringUtil {
 		}
 	}
 	
-	/**
-	 * 判断是否不是空
-	 * @param str
-	 * @return
-	 */
+
 	public static boolean isNotEmpty(String str){
 		if((str!=null)&&!"".equals(str.trim())){
 			return true;
@@ -36,11 +24,7 @@ public class StringUtil {
 		}
 	}
 	
-	/**
-	 * 格式化模糊查询
-	 * @param str
-	 * @return
-	 */
+
 	public static String formatLike(String str){
 		if(isNotEmpty(str)){
 			return "%"+str+"%";
@@ -49,11 +33,7 @@ public class StringUtil {
 		}
 	}
 	
-	/**
-	 * 过滤掉集合里的空格
-	 * @param list
-	 * @return
-	 */
+
 	public static List<String> filterWhite(List<String> list){
 		List<String> resultList=new ArrayList<String>();
 		for(String l:list){
