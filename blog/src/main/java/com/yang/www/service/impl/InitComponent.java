9cc3e0ec2934e925a58fbac16f20d884d41503cd
yang <1224213712@qq.com>
2017-08-29 08:37:15 +0800

    build InitComponent

--- blog/src/main/java/com/yang/www/service/impl/InitComponent.java
+++ blog/src/main/java/com/yang/www/service/impl/InitComponent.java
@@ -1,4 +1,4 @@
-package com.java1234.service.impl;
+package com.yang.www.service.impl;
 
 import java.util.List;
 
@@ -6,52 +6,53 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import com.yang.www.service.*;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
-import com.java1234.entity.Blog;
-import com.java1234.entity.BlogType;
-import com.java1234.entity.Blogger;
-import com.java1234.entity.Link;
-import com.java1234.service.BlogService;
-import com.java1234.service.BlogTypeService;
-import com.java1234.service.BloggerService;
-import com.java1234.service.LinkService;
+import com.yang.www.po.Blog;
+import com.yang.www.po.BlogType;
+import com.yang.www.po.Blogger;
+import com.yang.www.po.Link;
+//import com.yang.service.BlogTypeService;
+//import com.java1234.service.BloggerService;
+import com.yang.www.service.LinkService;
 
+/**
+ * 灏嗕竴浜涘父鐢ㄧ殑淇℃伅鏀惧埌application涓紝澧炲己绯荤粺鎬ц兘
+ */
 @Component
 public class InitComponent implements ServletContextListener,ApplicationContextAware{
 
 	private static ApplicationContext applicationContext;
-	
+
 	public void contextInitialized(ServletContextEvent sce) {
+
 		ServletContext application=sce.getServletContext();
 		BloggerService bloggerService=(BloggerService) applicationContext.getBean("bloggerService");
-		Blogger blogger=bloggerService.find(); // 获取博主信息
+		Blogger blogger=bloggerService.find();
 		blogger.setPassword(null);
 		application.setAttribute("blogger", blogger);
-		
+
 		LinkService linkService=(LinkService) applicationContext.getBean("linkService");
-		List<Link> linkList=linkService.list(null); // 查询所有的友情链接信息
+		List<Link> linkList=linkService.getLinks(null);
 		application.setAttribute("linkList", linkList);
-		
+
 		BlogTypeService blogTypeService=(BlogTypeService) applicationContext.getBean("blogTypeService");
-		List<BlogType> blogTypeCountList=blogTypeService.countList(); // 查询博客类别以及博客的数量
+		List<BlogType> blogTypeCountList=blogTypeService.getBlogTypes();
 		application.setAttribute("blogTypeCountList", blogTypeCountList);
-		
+
 		BlogService blogService=(BlogService) applicationContext.getBean("blogService");
-		List<Blog> blogCountList=blogService.countList(); // 根据日期分组查询博客
+		List<Blog> blogCountList=blogService.groupByDate();
 		application.setAttribute("blogCountList", blogCountList);
 	}
 
 	public void contextDestroyed(ServletContextEvent sce) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		this.applicationContext=applicationContext;
 	}
-
 }
