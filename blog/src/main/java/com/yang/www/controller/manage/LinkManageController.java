7805ac837710310f4b8f19fc6971d72864bac153
yang <1224213712@qq.com>
2017-08-29 09:05:06 +0800

    build LinkManageController for managing LinkInfo in back-end pages

--- blog/src/main/java/com/yang/www/controller/manage/LinkManageController.java
+++ blog/src/main/java/com/yang/www/controller/manage/LinkManageController.java
@@ -1,5 +1,31 @@
 package com.yang.www.controller.manage;
 
-
+import com.yang.www.service.LinkService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.yang.www.po.Link;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+@Controller
+@RequestMapping("/manage/link")
 public class LinkManageController {
+
+    @Resource
+    private LinkService linkService;
+
+    @RequestMapping("/list")
+    public void list(@RequestParam(value="page",required=false)Integer page,@RequestParam(value="rows",required=false)Integer rows,HttpServletResponse response){
+        linkService.list(page,rows,response);
+    }
+
+    @RequestMapping("/save")
+    public void save(Link link,HttpServletResponse response){
+        linkService.save(link,response);
+    }
+
+    @RequestMapping("/delete")
+    public void delete(@RequestParam(value="ids",required=false)String ids, HttpServletResponse response){
+        linkService.delete(ids,response);
+    }
 }
