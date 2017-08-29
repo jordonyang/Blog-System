8ea44120c0d35aac33eb4d11bd12ff449d812c36
yang <1224213712@qq.com>
2017-08-29 08:54:06 +0800

    build IndexController to show index.html

--- blog/src/main/java/com/yang/www/controller/IndexController.java
+++ blog/src/main/java/com/yang/www/controller/IndexController.java
@@ -1,4 +1,36 @@
 package com.yang.www.controller;
 
+import com.yang.www.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/")
 public class IndexController {
+
+    private BlogService blogService;
+
+    @Autowired
+    public void setBlogService(BlogService blogService) {
+        this.blogService = blogService;
+    }
+
+    /**
+     * 前台-伪静态形式获取博客列表
+     * @param page
+     * @param typeId
+     * @param releaseDateStr
+     * @param request
+     * @return
+     */
+    @RequestMapping("/index")
+    public ModelAndView index(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "typeId", required = false)
+            Integer typeId, @RequestParam(value = "releaseDateStr",
+            required = false) String releaseDateStr, HttpServletRequest request) {
+        return blogService.getList(page, typeId, releaseDateStr, request);
+    }
 }
