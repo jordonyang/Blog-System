47da6afc3d208b1e74d3fbfed036148d8ef308b7
yang <1224213712@qq.com>
2017-08-29 08:42:17 +0800

    build CryptographyUtil

--- blog/src/main/java/com/yang/www/utils/CryptographyUtil.java
+++ blog/src/main/java/com/yang/www/utils/CryptographyUtil.java
@@ -1,4 +1,13 @@
 package com.yang.www.utils;
 
+import org.apache.shiro.crypto.hash.Md5Hash;
+
 public class CryptographyUtil {
+    public static String encode(String str,String salt){
+        return new Md5Hash(str,salt).toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(CryptographyUtil.encode("1","blog"));;
+    }
 }
